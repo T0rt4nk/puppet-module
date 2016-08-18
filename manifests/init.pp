71df197ffd0058f0ad6eafcecd4bac1a7f429aff
Maxime Vidori <maxime.vidori@gmail.com>
2016-08-18 18:33:30 +0200

    Use motd plugin to setup the banner of tortank

--- manifests/init.pp
+++ manifests/init.pp
@@ -43,9 +43,7 @@
 # Copyright 2016 Your name here, unless otherwise noted.
 #
 class tortank {
-  file {'/tmp/example-ip':                                            # resource type file and filename
-    ensure  => present,                                               # make sure it exists
-    mode    => '0644',                                                # file permissions
-    content => "Here is my Public IP Address: ${ipaddress_eth0}.\n",  # note the ipaddress_eth0 fact
+  class { 'motd':
+    template => 'tortank/motd.erb',
   }
 }
